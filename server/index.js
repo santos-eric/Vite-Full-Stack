ce0626736a1b99dc1db4b12c33868ce22f1bffe1
santos-eric <esantosdev77@gmail.com>
2023-03-07 06:02:35 -0700

    first commit, node with deps, frontend with vite, no deps, no errors

---
+++ server/index.js
@@ -0,0 +1,7 @@
+// updated node doesn't require this format anymore, must enable at package.json below 'main': "index.js"
+// const express = require('express')
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
