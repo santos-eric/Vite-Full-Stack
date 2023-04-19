b1a24e790bd8c8df4565dcc6eacac1c5ed10889e
santos-eric <esantosdev77@gmail.com>
2023-04-19 04:47:15 -0600

    test

---
+++ server/controllers/posts.js
@@ -0,0 +1,15 @@
+// this controllers hold logic for the routes
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+	try {
+		const postMessages = await PostMessage.find();
+		res.status(200).json(postMessages);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+};
+
+export const createPost = (req, res) => {
+	res.send('Post Creation');
+};
